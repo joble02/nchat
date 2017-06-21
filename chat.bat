4efef1f92c8357aa288a25aafcc4ecbfe47a40d8
joble02 <joble02@yahoo.com>
2017-06-21 11:09:44 -0400

    Add files via upload

---
+++ chat.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo press 1, then enter for server
+echo press 2, then enter for client
+set /p server=$ 
+if %server% == 1 goto :server else goto :client
+
+:client
+
+mode con: cols=82 lines=1
+
+set /p ip=ip of server? 
+set /p port=port of server? 
+set /p user=Username? 
+
+start cmd /k "echo This is the display window, go back to the first one to send messages & ncat %ip% %port%"
+
+:loop
+set /p towrite=$ 
+echo #%user%# %towrite% | ncat %ip% %port% -i 10
+goto :loop
+
+:server
+echo Port? 
+set /p port=$ 
+
+ipconfig
+echo Under ipv4 is the address to give your friends
+echo The server is running, you don't need to touch this prompt anymore
+
+ncat --broker -p %port%
